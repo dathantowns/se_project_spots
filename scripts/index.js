55d170f073b1ed29471fbe0e0c64509e3fd628e3
Dathan Towns <dathantowns@gmail.com>
2025-02-25 13:10:06 -0800

    add modal html/css

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ const initialCards = [
   {
     name: "Dubai"
     link: "https://images.unsplash.com/photo-1732530365779-dbf5bdf4bc63?q=80&w=2160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  }
   {
     name: "Tokyo"
     link: "https://plus.unsplash.com/premium_photo-1661914240950-b0124f20a5c1?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
