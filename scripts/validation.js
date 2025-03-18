1343ab96450386c3f3d35f6d3b836e947a272618
Dathan Towns <dathantowns@gmail.com>
2025-03-17 17:28:08 -0700

    feat: add validation.js/set up config

---
+++ scripts/validation.js
@@ -0,0 +1,8 @@
+const settings = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save-btn",
+  inactiveButtonClass: "modal__save-btn_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__input-error_visisble",
+};
